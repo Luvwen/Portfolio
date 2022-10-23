a0531d2e8bed9f33ef4b7f479a2cd412696238fb
Santiago <santiagobalino@hotmail.com>
2022-10-23 06:46:01 -0300

    Data file to storage projects

--- src/data/data.js
+++ src/data/data.js
@@ -5,32 +5,50 @@ import phoneImg from './images/phone.png'
 export const myProjects = [
   {
     title: 'Heroes App',
-    text: 'Texto de Heroes App',
+    text: 'Texto de Heroes App y un poquito mas de info para rellenar para la próxima con algúna cosilla',
     image: heroesImg,
     technologies: ['javascript', 'react'],
     links: [
-      { github: 'https://github.com/Luvwen/HeroesApp-with-React' },
-      { netlify: 'https://luvwen-heroapp-react.netlify.app/' }
+      {
+        platformName: 'Github',
+        platformLink: 'https://github.com/Luvwen/HeroesApp-with-React'
+      },
+      {
+        platformName: 'Netlify',
+        platformLink: 'https://luvwen-heroapp-react.netlify.app/'
+      }
     ]
   },
   {
     title: 'Cryptocurrency App',
-    text: 'Texto de Cryptocurrency App',
+    text: 'Texto de Cryptocurrency App y un poquito mas de info para rellenar para la próxima con algúna cosilla',
     image: cryptoImg,
     technologies: ['javascript', 'react', 'redux'],
     links: [
-      { github: 'https://github.com/Luvwen/Cryptocurrency-App' },
-      { netlify: 'https://luvwen.github.io/Cryptocurrency-App/' }
+      {
+        platformName: 'Github',
+        platformLink: 'https://github.com/Luvwen/Cryptocurrency-App'
+      },
+      {
+        platformName: 'Github Pages',
+        platformLink: 'https://luvwen.github.io/Cryptocurrency-App/'
+      }
     ]
   },
   {
     title: 'Phone store',
-    text: 'Texto de Phone store',
+    text: 'Texto de Phone store y un poquito mas de info para rellenar para la próxima con algúna cosilla',
     image: phoneImg,
     technologies: ['javascript', 'react'],
     links: [
-      { github: 'https://github.com/Luvwen/phone-shop-balino' },
-      { netlify: 'https://phone-app-balino.netlify.app' }
+      {
+        platformName: 'Github',
+        platformLink: 'https://github.com/Luvwen/phone-shop-balino'
+      },
+      {
+        platformName: 'Netlify',
+        platformLink: 'https://phone-app-balino.netlify.app'
+      }
     ]
   }
 ]
